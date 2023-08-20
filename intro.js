442393dd107df6b9161e77aa1bbc3aa2e9a53302
marilin1999 <140349328+marilin1999@users.noreply.github.com>
2023-08-20 15:55:20 -0500

    Add files via upload

---
+++ intro.js
@@ -0,0 +1,17 @@
+let variable = 3
+let string = "marilin"
+let x = "3"
+let boolean = "3" + 5
+let igual = boolean
+
+ //         0     1         2      3   4
+let array =[2, "marilin", false, "p", 3.5]
+
+
+
+
+console.log(variable)
+console.log(string)
+console.log(x)
+console.log(boolean)
+console.log(array[0])
